22f57430c0935383ea3fa95694d910804b0ff22b
Muhammad Fahreza Azka Arafat <muhammad.fahreza11@ui.ac.id>
2024-10-23 09:45:46 +0700

    Feat init

---
+++ src/main/java/me/pick/metrodata/MetrodataApplication.java
@@ -0,0 +1,13 @@
+package me.pick.metrodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrodataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetrodataApplication.class, args);
+	}
+
+}
