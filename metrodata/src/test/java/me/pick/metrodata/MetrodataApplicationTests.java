956303dc331b2cef2b43f44aafb0ba6f7fa8a29e
Muhammad Fahreza Azka Arafat <muhammad.fahreza11@ui.ac.id>
2024-10-23 09:41:22 +0700

    Feat: Init

---
+++ metrodata/src/test/java/me/pick/metrodata/MetrodataApplicationTests.java
@@ -0,0 +1,13 @@
+package me.pick.metrodata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetrodataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
