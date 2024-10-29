9f2a52240ee4b65609d260bf767f467b06e4b25d
Muhammad Fahreza Azka Arafat <muhammad.fahreza11@ui.ac.id>
2024-10-29 09:03:13 +0700

    Chore(Bugfix): Added missing import statement on IS class

--- src/main/java/me/pick/metrodata/models/entity/InterviewSchedule.java
+++ src/main/java/me/pick/metrodata/models/entity/InterviewSchedule.java
@@ -3,6 +3,7 @@ package me.pick.metrodata.models.entity;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 
 import jakarta.persistence.*;
 
